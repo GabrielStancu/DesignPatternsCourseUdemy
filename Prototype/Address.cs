259b1d8a2f9c95ad09e45dda926d64863dc263f4
GabrielStancu <gabriel.stancu07@yahoo.com>
2021-07-08 15:37:52 +0300

    Added the Prototype DP

---
+++ Prototype/Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Address
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address()
+        {
+
+        }
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName;
+            HouseNumber = houseNumber; 
+        }
+
+        public Address(Address other)
+        {
+            StreetName = other.StreetName;
+            HouseNumber = other.HouseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+}
---
+++ Prototype/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Person
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person()
+        {
+
+        }
+
+        public Person(string[] names, Address address)
+        {
+            Names = names;
+            Address = address;
+        }
+
+        public Person(Person other)
+        {
+            Names = other.Names;
+            Address = new Address(other.Address);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(" ", Names)}, {nameof(Address)}: {Address}";
+        }
+    }
+}
